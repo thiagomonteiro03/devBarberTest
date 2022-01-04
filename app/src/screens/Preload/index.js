2e98a735d8aea98fbcdca433187fda8574d484e6
Thiago Nunes Monteiro <thiagomonteiro.trab@outlook.com>
2022-01-04 17:50:43 -0300

    Creating load screen and logic to move to sigin screen

---
+++ app/src/screens/Preload/index.js
@@ -0,0 +1,30 @@
+import React, {useEffect} from 'react';
+import {Text} from 'react-native';
+import {Container, LoadingIcon} from './styles';
+import AsyncStorage from '@react-native-community/async-storage';
+import {useNavigation} from '@react-navigation/native';
+
+import BarberLogo from '../../assets/barber.svg';
+
+export default () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem('token');
+      if (token != null) {
+        //valid token
+      } else {
+        navigation.navigate('SignIn');
+      }
+    };
+    checkToken();
+  }, [navigation]);
+
+  return (
+    <Container>
+      <BarberLogo width="100%" height="160" />
+      <LoadingIcon size="large" color="#FFFFFF" />
+    </Container>
+  );
+};
